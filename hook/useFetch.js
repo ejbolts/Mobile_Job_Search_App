672a49a431f6c1748f8aa15624f2f9ad0b6a335d
ethan <ejbolts@hotmail.com>
2023-08-17 14:06:45 +1000

    added usefech function to pull data, created popularjobs component

---
+++ hook/useFetch.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const options = {
+        method: "GET",
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+            "X-RapidAPI-Key": "3a0fec5acfmshcdec76bc999667ap1c9b12jsna821ecfc6e76", // Ideally should be in env file, but will keep it here to showcase the app working
+            "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+        },
+        params: { ...query },
+    };
+
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.request(options);
+
+            setData(response.data.data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+            console.log(error)
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    };
+
+    return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
